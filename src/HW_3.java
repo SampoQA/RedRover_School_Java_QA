56182e28e9f010f73d37551bb4f149328fdb3295
polya <linnahuo@gmail.com>
2026-09-07 20:21:22 +0300

    use &&

--- src/HW_3.java
+++ src/HW_3.java
@@ -2,13 +2,22 @@ public class HW_3 {
 
     public static void main(String[] args) {
         int a = 5;
-        int b = 2;
 
-        if ((a + b) % 2 == 0) {
-            IO.println("четное");
+        if (a > 10) {
+            System.out.println("больше 10");
         } else {
-            IO.println("нечётное");
+            if (a > 100) {
+                System.out.println("больше 100");
+            } else {
+                if (a / 2 < 20) {
+                    System.out.println("результат деления на 2 больше 20");
+                }
+                if (a < 40 && a > 5) {
+                    // Это условие не выполнится, потому что a=5 (a > 5 ложно)
+                } else {
+                    System.out.println("значение переменной между 5 и 40 включительно");
+                }
+            }
         }
-    }
-}
-
+    }   // ← закрываем метод main
+}       // ← закрываем класс HW_3
