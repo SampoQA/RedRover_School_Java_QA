c6beeed64ed28151b63d1ed46cbe61447133c503
polya <linnahuo@gmail.com>
2026-09-07 18:20:34 +0300

    Start

---
+++ src/HW_3.java
@@ -0,0 +1,17 @@
+public class HW_3 {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+
+        if (a == b) {
+            IO.println("a == b");
+        } else {
+            if (a < b) {
+                IO.println("a < b");
+            } else {
+                IO.println("a > b");
+            }
+        }
+    }
+}
