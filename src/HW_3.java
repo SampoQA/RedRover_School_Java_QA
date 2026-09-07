1f21939091ba2068b9b5c5fd1f5fabbfb6d4e682
polya <linnahuo@gmail.com>
2026-09-07 18:40:39 +0300

    sum even

--- src/HW_3.java
+++ src/HW_3.java
@@ -2,16 +2,13 @@ public class HW_3 {
 
     public static void main(String[] args) {
         int a = 5;
-        int b = 3;
+        int b = 2;
 
-        if (a == b) {
-            IO.println("a == b");
+        if ((a + b) % 2 == 0) {
+            IO.println("четное");
         } else {
-            if (a < b) {
-                IO.println("a < b");
-            } else {
-                IO.println("a > b");
-            }
+            IO.println("нечётное");
         }
     }
 }
+
